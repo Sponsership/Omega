0f2f3be39fc72f1ad70c494e77ac3ba80ed26a67
Sponsership <74657732+Sponsership@users.noreply.github.com>
2020-11-18 17:14:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,16 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC-eMxaSJnPotxGCxU9Hkm4LeamYH03j1E",
+  authDomain: "store-71e2.firebaseapp.com",
+  databaseURL: "https://store-71e2.firebaseio.com",
+  projectId: "store-71e2",
+  storageBucket: "store-71e2.appspot.com",
+  messagingSenderId: "672931521096",
+  appId: "1:672931521096:web:ce034d28aebeab892bee7d",
+  measurementId: "G-GTKGLSB151"
 };
 
      
